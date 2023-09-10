46df260f9688ab70e5acdb6d3f779fb55b56e94b
ILikePlayingGames <22475143+ILikePlayingGames@users.noreply.github.com>
2023-09-10 00:36:29 -0400

    Add 21:9 layout and other improvements - Add dungeon hub to build 53 - Deduplicate code in build.gradle.kts

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# FancyWarpMenuResourcePacks
+# Fancy Warp Menu Resource Packs
 Resource packs that add alternative layouts and textures to the Fancy Warp Menu
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,33 @@
+import groovy.lang.Closure
+
 plugins {
     base
 }
 
 tasks.build {
-    dependsOn("build53", "buildUltraWide")
+    dependsOn(tasks.matching { it.group.equals("build resource pack") })
 }
 
 tasks.register<Zip>("build53") {
-    archiveBaseName.set("Fancy Warp Menu Build 53")
-    destinationDirectory.set(layout.buildDirectory)
-    from(layout.projectDirectory.dir("src/Fancy Warp Menu Build 53"))
-    filesMatching("pack.mcmeta") {
-        expand("modVersion" to project.properties["fancyWarpMenuVersion"])
-    }
+    configure(getTexturePackConfiguration("Fancy Warp Menu Build 53"))
 }
 
-tasks.register<Zip>("buildUltraWide") {
-    archiveBaseName.set("Fancy Warp Menu UltraWide")
-    destinationDirectory.set(layout.buildDirectory)
-    from(layout.projectDirectory.dir("src/Fancy Warp Menu UltraWide"))
-    filesMatching("pack.mcmeta") {
-        expand("modVersion" to project.properties["fancyWarpMenuVersion"])
+tasks.register<Zip>("build21by9") {
+    configure(getTexturePackConfiguration("Fancy Warp Menu 21:9"))
+}
+
+tasks.register<Zip>("build32by9") {
+    configure(getTexturePackConfiguration("Fancy Warp Menu 32:9"))
+}
+
+fun getTexturePackConfiguration(texturePackName: String): Closure<Any?> {
+    return closureOf<Zip> {
+        group = "build resource pack"
+        archiveBaseName.set(texturePackName)
+        destinationDirectory.set(layout.buildDirectory)
+        from(layout.projectDirectory.dir("src/${texturePackName}"))
+        filesMatching("pack.mcmeta") {
+            expand("modVersion" to project.properties["fancyWarpMenuVersion"])
+        }
     }
 }
